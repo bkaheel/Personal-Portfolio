90ef2ce453c3b665c2f1529c0bf2a4b9b321644a
BenjaminKaheel <benjaminkaheel1@gmail.com>
2024-10-30 10:05:39 -0400

    Update description of skills and icons

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -3,10 +3,11 @@ import AnimatedLetters from '../AnimatedLetters';
 import Contact from '../Contact';
 import './index.scss';
 import { useState, useEffect } from 'react';
-import { FaJava, FaPython, FaReact, FaSass } from 'react-icons/fa';
+import { FaAngular, FaJava, FaPython, FaReact, FaSass, FaSwift } from 'react-icons/fa';
 import { SiJavascript, SiTailwindcss } from 'react-icons/si';
 import { DiCss3, DiHtml5 } from 'react-icons/di';
 import { DiCode } from 'react-icons/di';
+import { RiJavascriptFill } from "react-icons/ri";
 
 const Skills = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -24,13 +25,13 @@ const Skills = () => {
   const logos = [
     { icon: FaJava, name: 'Java' },
     { icon: FaPython, name: 'Python' },
-    { icon: SiJavascript, name: 'JavaScript' },
+    { icon: RiJavascriptFill, name: 'JavaScript' },
+    { icon: FaSwift, name: 'Swift' },
     { icon: FaReact, name: 'React' },
+    { icon: FaAngular, name: 'Angular' },
     { icon: DiCode, name: 'C' },
     { icon: DiHtml5, name: 'HTML' },
-    { icon: DiCss3, name: 'CSS' },
-    { icon: FaSass, name: 'Sass' },
-    { icon: SiTailwindcss, name: 'TailwindCSS' },
+    { icon: DiCss3, name: 'CSS' }
   ];
   
   return (
@@ -41,17 +42,18 @@ const Skills = () => {
             <AnimatedLetters letterClass={letterClass} strArray={['S', 'k', 'i', 'l', 'l', 's']} idx={15} />
           </h1>
           <p>
-            Throughout my programming journey, embraced a diverse array of languages and technologies. From crafting backend 
-            applications to engaging frontend experiences, I've immersed myself in various tools and frameworks to achieve 
-            remarkable results. <span className='standout-text'>Java</span> remains the stronghold, yet countless 
-            hours dedicated to <span className='standout-text'>Python</span> as well. Great skillset encompassing <span className='standout-text'>HTML</span>, <span className='standout-text'>CSS</span>, <span className='standout-text'>React.js</span>, <span className='standout-text'>TailwindCSS</span>, and much more. 
-            This versatility empowers me to tackle diverse projects with precision and creativity.            
+            Before I came to UNC and began to take computer science courses, I had never touched any programming languages. Yet, I've learned many different 
+            languages at through coursework and enjoyed it, so much that I picked up more languages and technologies on my own time at home. Given that I'm 
+            a fast-learner, possess experience with problem-solving through my many managment roles, and can manage a significant workload, I have taken on 
+            frontend and backend languages and techonologies. I would be even more excited to learn more languages and technologies, expanding my skillset 
+            with your company. These are just a few of the programming languages and technologies I have worked with, but there are many more like <span className='standout-text'>Git</span>, <span className='standout-text'>APIs</span>, 
+            <span className='standout-text'>TypeScript</span>, etc.         
             </p>
             <p>
             Visit my <a 
                 target='_blank' 
                 rel='noreferrer' 
-                href='https://www.linkedin.com/in/benjamin-kaheel/'
+                href='https://www.linkedin.com/in/benkaheel/'
                 >
                     <span className='standout-link'>LinkedIn</span> 
                 </a> profile for more details or even <a 
