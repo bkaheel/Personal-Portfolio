7dd413540b6cf6fe531bd5d90787cd259ecf9aaf
BenjaminKaheel <benjaminkaheel1@gmail.com>
2024-10-30 10:38:50 -0400

    Upload resume and Link it

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -63,6 +63,7 @@ const Skills = () => {
                 rel='noreferrer' 
                 href='../Contact'
                 >
+                  <a href='Benjamin_Kaheel_Resume_.pdf'>ressummee</a>
                     <span className='standout-link'>contact</span>
                 </a> me for a copy of my resume. 
             </p>
